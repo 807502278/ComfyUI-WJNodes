0971989098d6055f30092dc330557657f8d139a9
807502278 <807502278@qq.com>
2025-05-05 20:21:00 +0800

    Add comments

--- nodes/other_nodes.py
+++ nodes/other_nodes.py
@@ -17,293 +17,12 @@ from ..moduel.image_utils import pil_to_mask
 # ------------------GetData nodes------------------
 CATEGORY_NAME = "WJNode/GetData"
 
-class Select_Images_Batch:
-    DESCRIPTION = """
-        返回指定批次编号处的图像(第1张编号为0,可以任意重复和排列组合)
-        超出范围的编号将被忽略，若输入为空则一个都不选，可识别中文逗号。\n
-        Return the image at the specified batch number (the first image is numbered 0, and can be arbitrarily repeated and combined) 
-        numbers out of range will be ignored, If the input is empty, none will be selected, Chinese commas can be recognized.
-    """
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "indexes": ("STRING", {"default":"1,2,","multiline": True}),
-            },
-            "optional": {
-                "images": ("IMAGE",),
-                "masks": ("MASK",),
-            },
-        }
-    CATEGORY = CATEGORY_NAME
-    RETURN_TYPES = ("IMAGE", "IMAGE","MASK","MASK")
-    RETURN_NAMES = ("select_img", "exclude_img","select_mask", "exclude_mask")
-    FUNCTION = "SelectImages"
-
-    def SelectImages(self, indexes, images = None, masks = None):
-        select_list = np.array(str_edit.tolist_v2(indexes, to_oneDim=True, to_int=True, positive=True))
-
-        #选择图像批次
-        if images is not None:
-            n_i = images.shape[0]
-            s_i = select_list[(select_list >= 1) & (select_list <= n_i)]-1
-            if len(s_i) < 1:  # 若输入的编号全部不在范围内则返回原输入
-                print("Warning:The input value is out of range, return to the original input.")
-                exclude_img, select_img = images, None
-            else:
-                e_i = np.setdiff1d(np.arange(0, n_i), s_i)  # 排除的图像
-                select_img = images[torch.tensor(s_i, dtype=torch.int)]
-                exclude_img = images[torch.tensor(e_i, dtype=torch.int)]
-        else:
-            select_img,exclude_img = None, None
-        
-        #选择遮罩批次
-        if masks is not None:
-            n_m = masks.shape[0]
-            s_m = select_list[(select_list >= 1) & (select_list <= n_m)]-1
-            if len(s_m) < 1:  # 若输入的编号全部不在范围内则返回原输入
-                print("Warning:The input value is out of range, return to the original input.")
-                exclude_mask, select_mask = masks, None
-            else:
-                e_m = np.setdiff1d(np.arange(0, n_m), s_m)  # 排除的图像
-                select_mask = masks[torch.tensor(s_m, dtype=torch.int)]
-                exclude_mask = masks[torch.tensor(e_m, dtype=torch.int)]
-        else:
-            select_mask,exclude_mask = None, None
-
-        return (select_img, exclude_img,
-                select_mask, exclude_mask)
-
-
-class Select_Batch_v2:
-    DESCRIPTION = """
-        功能：
-        返回指定批次编号处的图像，第1张编号为0,可以任意重复和排列组合
-        指定的批次编号可按一定规则处理
-        输入参数：
-        indexes：若为空则为全选，可识别中文逗号。
-        loop：将选择的批次复制指定次数
-        loop_method：批次复制方式，tile直接增加，repeat每个编号往后复制loop个
-        limit：超出范围的编号处理方式，Clamp钳制到最大批次内，Loop在最大批次内循环，ignore忽略
-        images/masks：图像遮罩批次，批次数量可以不相同
-        输出参数：
-        select_img/exclude_img：选择/反选的图像(反选的将不进行批次处理)
-        select_mask/exclude_mask：反选的遮罩(反选的将不进行批次处理)
-        img_order/mask_order：选择的图像/遮罩批次原编号数据\n
-        Functionality:
-        Return the image at the specified batch number, with the first image numbered as 0, allowing for arbitrary repetition and combination.
-        The specified batch numbers can be processed according to certain rules.
-        Input parameters:
-        indexes: If empty, select all; capable of recognizing Chinese commas.
-        loop: The number of times to duplicate the selected batches.
-        loop_method: The method of batch duplication, with 'tile' directly adding, and 'repeat' copying each number backward by the loop count.
-        limit: The handling method for numbers out of range, 'Clamp' restricts to the maximum batch, 'Loop' cycles within the maximum batch, and 'ignore' disregards them.
-        images/masks: Image/mask batches, which can have varying quantities.
-        Output parameters:
-        select_img/exclude_img: Selected/inverse-selected images (inverse-selected will not undergo batch processing).
-        select_mask/exclude_mask: Inverse-selected masks (inverse-selected will not undergo batch handling).
-        img_order/mask_order: The original batch number data of the selected images/masks.
-    """
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "indexes": ("STRING", {"default": "1,2,","multiline": True}),
-                "loop": ("INT", {"default":1,"min":1,"max":2048}),
-                "loop_method": (["tile","repeat"],{"default":"tile"}),
-                "limit": (["Clamp","Loop","ignore"],{"default":"Clamp"}),
-            },
-            "optional": {
-                "images": ("IMAGE",),
-                "masks": ("MASK",),
-            },
-        }
-    CATEGORY = CATEGORY_NAME
-    RETURN_TYPES = ("IMAGE", "IMAGE","MASK","MASK","LIST","LIST",)
-    RETURN_NAMES = ("select_img", "exclude_img","select_mask", "exclude_mask","img_order","mask_order")
-    FUNCTION = "SelectImages"
-
-    def SelectImages(self, indexes, loop, loop_method, limit, images=None, masks=None):
-        #初始值
-        data_none = [None,None]
-        img_order = []
-        mask_order = []
-        select_list = np.array(str_edit.tolist_v2(indexes, to_oneDim=True, to_int=True, positive=True))
-
-        #如果选择列表为空值则直接输出
-        n_s = len(select_list) 
-        if n_s == 0 : 
-            print("Warning:No valid input detected, original data will be output!")
-            return (images,None,masks,None,img_order,mask_order)
-
-        #选择图像
-        if images is not None : 
-            img_order = self.handle_data(select_list,loop,images.shape[0],limit,loop_method,indexes)
-            select_img,exclude_img,_ = self.select_data(images,img_order)
-        else: 
-            print("Warning: Image input is empty, output will be empty")
-            select_img,exclude_img = data_none
-
-        #选择遮罩
-        if masks is not None : 
-            mask_order = self.handle_data(select_list,loop,masks.shape[0],limit,loop_method,indexes)
-            select_mask,exclude_mask,_ = self.select_data(masks,img_order)
-        else:
-            print("Warning: Mask input is empty, output will be empty")
-            select_mask,exclude_mask = data_none
-
-        return (select_img, exclude_img, 
-                select_mask, exclude_mask, 
-                img_order, mask_order)
-    
-    #处理选择数据
-    def handle_data(self,select_list,loop,n,limit,loop_method,indexes):
-        """
-        select_list：原选择列表
-        loop：循环复制数量
-        n:原批次总数
-        limit：超过的序号补充方式(3种)
-        loop_method：循环复制的方式
-        indexes:输入的原字符串编号，若为""则为全选
-
-        返回：处理后的选择列表
-        """
-        if indexes == "":
-            select_list = np.arange(0,n)
-        if limit == "Loop": #将超过最大批次的序号在最大批次内循环
-            if loop_method == "tile":
-                return np.mod(np.tile(select_list,loop),n)
-            elif loop_method == "repeat":
-                return np.mod(np.repeat(select_list,loop),n)
-        elif limit == "Clamp": #钳制超过最大批次的序号到最大批次以内
-            if loop_method == "tile":
-                return np.clip(np.tile(select_list,loop),0,n-1)
-            elif loop_method == "repeat":
-                return np.clip(np.repeat(select_list,loop),0,n-1)
-        elif limit == "ignore": #忽略超过最大批次的序号
-            if indexes != "":
-                select_list = select_list[(select_list >= 1) & (select_list <= n)]-1
-            if loop_method == "tile":
-                return np.tile(select_list,loop)
-            elif loop_method == "repeat":
-                return np.repeat(select_list,loop)
-        else:
-            return None
-    
-    #选择批次
-    def select_data(self, t, list):
-        """
-        参数：
-        t:批次图像/遮罩
-        list:选择编号列表
-
-        返回: 
-        e_s: 反选编号列表
-        t:重新组合的批次数据
-        e_t:反选批次数据
-        """
-        e_s = np.setdiff1d(np.arange(0, t.shape[0]), list)
-        e_t = t[torch.tensor(e_s, dtype=torch.int)]
-        t = t[torch.tensor(list, dtype=torch.int)]
-        return(t,e_t,e_s)
-
-
-class SelectBatch_paragraph: # ******************开发中
-    DESCRIPTION = """
-    功能：
-    返回指定批次段(第一张图像编号为0)
-    独立计算图像和遮罩输入(批次和大小可以不同)
-    输入参数：
-    Item：起始批次编号(包含此编号的图像)，为负数时从后面计数
-    length：选择批次长度，默认从后面计数，为负数时从前面计数
-    extend：当选择长度超过第一或最后一张时的填充方式
-    *extend-no_extend：不填充，
-    *extend-start_extend：填充第一张到指定长度
-    *extend-end_extend：填充最后一张到指定长度
-    *extend-loop：循环填充到指定长度
-    *extend-loop_mirror：填充镜像循环填充到指定长度
-    reversal_batch：反转批次
-    输入参数：
-    select_img/exclude_img：选择的图像批次/排除的图像批次
-    select_mask/exclude_mask：选择的遮罩批次/排除的遮罩批次
-    """
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "Item": ("INT", {"default": 0,"min":-4096,"max":4096}),
-                "length": ("INT", {"default": 1,"min":-4096,"max":4096}),
-                "extend": (["no_extend","start_extend","end_extend","loop","loop_mirror"],{"defailt":"no_extend"}),
-                "reversal_batch": ("BOOLEAN",{"default":False}),
-            },
-            "optional": {
-                "images": ("IMAGE",),
-                "masks": ("MASK",),
-            },
-        }
-    CATEGORY = CATEGORY_NAME
-    RETURN_TYPES = ("IMAGE", "IMAGE","MASK","MASK",)
-    RETURN_NAMES = ("select_img", "exclude_img","select_mask", "exclude_mask")
-    FUNCTION = "SelectImages"
-
-    def SelectImages(self, images, mode1_indexes):
-        select_list = np.array(str_edit.tolist_v2(
-            mode1_indexes, to_oneDim=True, to_int=True, positive=True))
-        select_list1 = select_list[(select_list >= 1) & (select_list <= len(images))]-1
-        if len(select_list1) < 1:  # 若输入的编号全部不在范围内则返回原输入
-            print(
-                "Warning:The input value is out of range, return to the original input.")
-            return (images, None)
-        else:
-            exclude_list = np.arange(1, len(images) + 1)-1
-            exclude_list = np.setdiff1d(exclude_list, select_list1)  # 排除的图像
-            if len(select_list1) < len(select_list):  # 若输入的编号超出范围则仅输出符合编号的图像
-                n = abs(len(select_list)-len(select_list1))
-                print(
-                    f"Warning:The maximum value entered is greater than the batch number range, {n} maximum values have been removed.")
-            print(f"Selected the first {select_list1} image")
-            return (images[torch.tensor(select_list1, dtype=torch.float)], 
-                    images[torch.tensor(exclude_list, dtype=torch.float)])
- 
-
-class Batch_Average: # ******************开发中
-    DESCRIPTION = """
-    功能：
-    将批次平均切割
-    输入：
-    Item：分段开始序号
-    division：分段数
-    select：选择输出第几段
-    """
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "Item": ("INT", {"default": 0,"min":-4096,"max":4096}),
-                "division": ("INT", {"default": 1,"min":1,"max":4096}),
-                "select": ("INT", {"default": 1,"min":-4096,"max":4096}),
-            },
-            "optional": {
-                "images": ("IMAGE",),
-                "masks": ("MASK",),
-            },
-        }
-    CATEGORY = CATEGORY_NAME
-    RETURN_TYPES = ("IMAGE", "IMAGE","MASK","MASK",)
-    RETURN_NAMES = ("select_img", "exclude_img","select_mask", "exclude_mask")
-    FUNCTION = "SelectImages"
-
-    def SelectImages(self, images):
-        ...
-
-
 class Mask_Detection:
     DESCRIPTION = """
-
+    中文/CH
     说明：
         输入mask或image,使用去重检测遮罩属性
         若mask或image都输入，默认检测image
-
     输入：
         1:Exist_threshold判断遮罩是否存在的阈值，
     输出：
@@ -313,6 +32,14 @@ class Mask_Detection:
         4:是否为全黑遮罩
         5:是否为灰度遮罩
         6:输出色值(当mask不为单色时输出0)
+    EN/英文
+    Description:
+        Input mask or image, use unique detection to check the mask attributes
+        If both mask and image are input, the default is to detect image
+    Input:
+        1:Exist_threshold, the threshold for determining if the mask exists
+    Output:
+        1:Whether the mask exists (not pure color)
     """
 
     @classmethod
@@ -416,6 +143,16 @@ CATEGORY_NAME = "WJNode/Other-node"
 
 
 class Any_Pipe: # 任意数据打组
+    DESCRIPTION = """
+    中文/CH
+    将任意数据打包成一个列表以减少面条
+    自带拆分数据，可嵌套
+
+    EN/英文
+    Any data grouping
+    Pack any data into a list to reduce noodles
+    It has data splitting, and can be nested
+    """
     @classmethod
     def INPUT_TYPES(s):
         return {
@@ -538,7 +275,25 @@ class Folder_link: # 创建符号链接 ******************开发中
                 p_link.symlink_to(p)
 
 
-class Determine_Type:
+class Determine_Type: # 判断输入的数据类型是否和所选类型匹配
+    DESCRIPTION = """
+    中文/CH
+    判断输入的数据类型是否和所选类型匹配并输出数据类型
+    类型含图像、张量、模型、采样器、基础类型：
+        图像类型含255、4通道、3通道、批次图像、单张图像
+        张量类型含bool、int、float
+        模型类型含unet、vae、clip
+        采样器类型含conditioning、latent、latent_noise_mask
+        基础类型含None、int、float、string、complex、tuple、list、set、dict、Tensor
+    EN/英文
+    Determine if the input data type matches the selected type and output the data type
+    The type includes image, tensor, model, sampler, and basic type:
+        The image type includes 255, 4 channels, 3 channels, batch images, and single images
+        The tensor type includes bool, int, float
+        The model type includes unet, vae, clip
+        The sampler type includes conditioning, latent, latent_noise_mask
+        The basic type includes None, int, float, string, complex, tuple, list, set, dict, Tensor
+    """
     type_dict = {
         "comfyui/image": ["torch.HalfTensor","torch.BFloat16Tensor","torch.FloatTensor"],
         "comfyui/image255": ["torch.ByteTensor","torch.CharTensor","torch.ShortTensor"],
@@ -1182,13 +937,11 @@ class Sam2AutoSegmentation_data:
 
 NODE_CLASS_MAPPINGS = {
     #WJNode/GetData
-    "Select_Images_Batch": Select_Images_Batch,
-    "Select_Batch_v2": Select_Batch_v2,
     "Mask_Detection": Mask_Detection,
+    "get_image_data": get_image_data,
     #WJNode/Other-functions
     "Any_Pipe": Any_Pipe,
     "Determine_Type": Determine_Type,
-    "get_image_data": get_image_data,
     #WJNode/Other-plugins
     "WAS_Mask_Fill_Region_batch": WAS_Mask_Fill_Region_batch,
     "SegmDetectorCombined_batch": SegmDetectorCombined_batch,
